1ab0c5fc0286194b3e5470e6b8395328195c010e
jayjayppark <150012836+jayjayppark@users.noreply.github.com>
2024-09-26 12:58:28 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,48 +1,39 @@
-import java.awt.image.renderable.ContextualRenderedImageFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-import java.util.ArrayDeque;
-
 public class Main {
     public static void main(String[] arg) throws IOException {
         StringBuilder sb = new StringBuilder();
-        ArrayDeque<Character> ad = new ArrayDeque<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         boolean noVPS;
+        int temp;
         for (int i = 0; i < T; i++) {
+            temp = 0;
             noVPS = true;
             String curStr = br.readLine();
             for (int j = 0; j < curStr.length(); j++) {
-                char cur = curStr.charAt(j);
-                if (cur == '(') {
-                    ad.push(cur);
+                if (curStr.charAt(j) == '(') {
+                    temp++;
                 } else {
-                    if (ad.isEmpty()) {
+                    if (temp == 0) {
                         sb.append("NO\n");
                         noVPS = false;
                         break;
                     } else {
-                        if (ad.peek() == ')') {
-                            sb.append("NO\n");
-                            noVPS = false;
-                            break;
-                        } else {
-                            ad.pop();
-                        }
+                        temp--;
                     }
                 }
             }
             if (noVPS) {
-                if (ad.isEmpty()) {
-                    sb.append("YES\n");
-                } else {
+                if (temp != 0) {
                     sb.append("NO\n");
+                } else {
+                    sb.append("YES\n");
                 }
             }
-            ad.clear();
+
         }
         System.out.print(sb.toString());
     }
