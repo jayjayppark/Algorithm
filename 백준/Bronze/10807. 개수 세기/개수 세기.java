f32c21d8cb15a33ed9b03a5b9ec9b3ad27fae268
jayjayppark <150012836+jayjayppark@users.noreply.github.com>
2024-09-26 19:08:21 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,17 +2,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int v = Integer.parseInt(br.readLine());
+        String[] splitNums = br.readLine().split(" ");
+        String v = br.readLine();
         int solution = 0;
         for (int i = 0; i < N; i++) {
-            if (v == Integer.parseInt(st.nextToken())) {
+            if (v.equals(splitNums[i])) {
                 solution++;
             }
         }
