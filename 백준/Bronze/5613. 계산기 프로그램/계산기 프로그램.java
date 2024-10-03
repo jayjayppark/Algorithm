8d93139887d84ea07c00d2087dd3fd18909c2ceb
jayjayppark <150012836+jayjayppark@users.noreply.github.com>
2024-10-03 18:03:02 +0900

    [Bronze III] Title: 계산기 프로그램, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tmp;
+        tmp = Integer.parseInt(br.readLine());
+        char cur = br.readLine().charAt(0);
+        while (cur != '=') {
+            if (cur == '+') {
+                tmp += Integer.parseInt(br.readLine());
+            } else if (cur == '-') {
+                tmp -= Integer.parseInt(br.readLine());
+            } else if (cur == '*') {
+                tmp *= Integer.parseInt(br.readLine());
+            } else if (cur == '/') {
+                tmp /= Integer.parseInt(br.readLine());
+            }
+            cur = br.readLine().charAt(0);
+        }
+        System.out.print(tmp);
+    }
+}
