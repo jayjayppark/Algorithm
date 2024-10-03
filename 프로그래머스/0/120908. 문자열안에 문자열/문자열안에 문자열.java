4343eac4d21cba5ae2003582852c95fd203d5cf9
jayjayppark <150012836+jayjayppark@users.noreply.github.com>
2024-10-03 20:05:07 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.36 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,20 +1,59 @@
 class Solution {
     public int solution(String str1, String str2) {
-        boolean isSame;
-        for (int i = 0; i <= str1.length() - str2.length(); i++) {
-            if (str1.charAt(i) == str2.charAt(0)) {
-                isSame = true;
-                for (int j = 1; j < str2.length(); j++) {
-                    if (str1.charAt(i + j) != str2.charAt(j)) {
-                        isSame = false;
-                        break;
-                    }
-                }
-                if (isSame == true) {
-                    return 1;
+        return KMPAlgorithm.KMPSearch(str2, str1);
+    }
+    
+    
+}
+
+class KMPAlgorithm {
+    public static int KMPSearch(String pat, String txt) {
+        int m = pat.length();
+        int n = txt.length();
+        
+        int[] lps = new int[m];
+        computeLPSArray(pat, m, lps);
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < n) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+            
+            if (j == m) {
+                return 1;
+            } else if (i < n && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
                 }
             }
         }
         return 2;
+    }
+    
+    public static void computeLPSArray(String pat, int m, int[] lps) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+        
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
     }
 }
